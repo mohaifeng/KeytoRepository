9c98e0fb50a65b9933404a7a57752e570b38111d
mohaifeng <1093785431@qq.com>
2025-07-19 17:53:54 +0800

    1

--- STM32-G431RBT6/Core/Inc/led.h
+++ STM32-G431RBT6/Core/Inc/led.h
@@ -9,18 +9,25 @@
 #define INC_LED_H_
 
 #include "stm32g4xx_hal.h"
-// 颜色和闪烁状态变量
+// 颜色变量
 typedef enum
 {
-	COLOR_RED,//红色
-	COLOR_GREEN,//绿色
-	COLOR_BLUE,//蓝色
-	COLOR_YELLOW,//黄色
-	COLOR_CYAN,//青色
-	COLOR_MAGENTA,//紫色
-	COLOR_WHITE,//白色
-	COLOR_OFF//关闭
+	COLOR_RED, //红色
+	COLOR_GREEN, //绿色
+	COLOR_BLUE, //蓝色
+	COLOR_YELLOW, //黄色
+	COLOR_CYAN, //青色
+	COLOR_MAGENTA, //紫色
+	COLOR_WHITE, //白色
+	COLOR_OFF //关闭
 } ColorTypedef;
+
+typedef enum
+{
+	Led_On = 1,
+	Led_Off,
+	Led_flick
+} Led_Statue;
 
 void Led_SetCorlor(ColorTypedef corlor);
 void Led_Init(void);
